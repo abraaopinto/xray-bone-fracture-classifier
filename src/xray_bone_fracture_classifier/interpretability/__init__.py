6565f95819dc96166a54dd3a71656cf3aa349de4
Abraão Pinto <abraao.pinto@basa.com.br>
2026-02-03 16:38:20 -0300

    chore(lint): configure ruff and fix interpretability re-exports

--- src/xray_bone_fracture_classifier/interpretability/__init__.py
+++ src/xray_bone_fracture_classifier/interpretability/__init__.py
@@ -1,1 +1,5 @@
-from .gradcam import GradCAM, GradCAMResult, overlay_cam_on_pil
+from .gradcam import GradCAM as GradCAM
+from .gradcam import GradCAMResult as GradCAMResult
+from .gradcam import overlay_cam_on_pil as overlay_cam_on_pil
+
+__all__ = ["GradCAM", "GradCAMResult", "overlay_cam_on_pil"]
